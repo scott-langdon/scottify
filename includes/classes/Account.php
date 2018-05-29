85d9c98c4c853432b962479dbf953f3d6a9de999
Scott Langdon <scott.langdon@tylt.com>
2018-05-28 18:47:23 -0700

    validation: username, fn, ln, and  email

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -23,15 +23,43 @@
 				return;
 			}
 
-			// TODO: check if username exists 
+			// TODO: check if username exists
 
 		}
 
-		private function validateFirstName($fn) {}
+		private function validateFirstName($fn) {
 
-		private function validateLastName($ln) {}
+			if(strlen($fn) > 25 || strlen($fn) < 2) {
+				array_push($this->errorArray, "Your first name must be between 2 and 25 charactars");
+				return;
+			}
 
-		private function validateEmails($em, $em2) {}
+		}
+
+		private function validateLastName($ln) {
+
+			if(strlen($ln) > 25 || strlen($ln) < 2) {
+				array_push($this->errorArray, "Your last name must be between 2 and 25 charactars");
+				return;
+			}
+
+		}
+
+		private function validateEmails($em, $em2) {
+
+			if($em != $em2) {
+				array_push($this->errorArray, "Your emails do not match");
+				return;
+			}
+
+			if(!filter_var($em, FILTER_VALIDATE_EMAIL)) {
+				array_push($this->errorArray, "Email is invalid");
+				return;
+			}
+
+			// TODO: Check that username hasn't already been used
+
+		}
 
 		private function validatePasswords($pw, $pw2) {}
 
