46bba360dd06c7fb5c8f0cb9ef351f86be3e472d
Scott Langdon <scott.langdon@tylt.com>
2018-05-28 18:36:55 -0700

    creating login/reg, creating validation for login/reg

---
+++ includes/handlers/register-handler.php
@@ -0,0 +1,34 @@
+<?php  
+
+function sanitizeFormPassword($inputText) {
+	$inputText = strip_tags($inputText);
+	return $inputText;
+}
+
+function sanitizeFormString($inputText) {
+	$inputText = strip_tags($inputText);
+	$inputText = str_replace(" ", "", $inputText);
+	$inputText = ucfirst(strtolower($inputText));
+	return $inputText;
+}
+
+function sanitizeFormUsername($inputText) {
+	$inputText = strip_tags($inputText); // removes any html elements so no one can manipulate site
+	$inputText = str_replace(" ", "", $inputText); // this will replace all the spaces to clean it up
+	return $inputText; 
+}
+
+if(isset($_POST['registerButton'])) {
+	//Register button was pressed
+	$username = sanitizeFormUsername($_POST['username']);
+	$firstName = sanitizeFormString($_POST['firstName']);
+	$lastName = sanitizeFormString($_POST['lastName']);
+	$email = sanitizeFormString($_POST['email']);
+	$email2 = sanitizeFormString($_POST['email2']);
+	$password = sanitizeFormPassword($_POST['password']);
+	$password2 = sanitizeFormPassword($_POST['password2']);
+
+	$account->register($username, $firstName, $lastName, $email, $email2, $password, $password2);
+}
+
+?>
